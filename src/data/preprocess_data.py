89e8f19340b9444f4dd8ecf442a80cfd9d5fc386
liuyuwe5 <liuyuwei012@gmail.com>
2023-06-26 13:42:06 -0400

    add: preprocess_time_string in preprocess_data.py

--- src/data/preprocess_data.py
+++ src/data/preprocess_data.py
@@ -3,23 +3,12 @@ import os
 import numpy as np
 from tqdm.auto import tqdm
 
-DATA_PATH = "VCHAMPS"
+from datetime import datetime
 
-# dataset
-# useful column: Internalpatientid, Age at admission, Admission date, 
-#                First listed discharge diagnosis icd10 subcategory, Second listed discharge diagnosis icd10 subcategory,
-#                Died during admission, Outpatientreferralflag
-dataset_path = 'inpatient_admissions_train.csv'
-load_path = os.path.join(DATA_PATH, dataset_path)
-in_ad = pd.read_csv(load_path)
-in_ad = in_ad[["Internalpatientid", "Age at admission", "Admission date",
-               "First listed discharge diagnosis icd10 subcategory", "Second listed discharge diagnosis icd10 subcategory",
-               "Died during admission", "Outpatientreferralflag"]]
+def preprocess_time_string(timeStr):
+    timeStr = timeStr[:-2]
+    date_format = '%Y-%m-%d %H:%M:%S'
+    date_obj = datetime.strptime(timeStr, date_format)
+    return date_obj
 
-# dataset
-# Useful columns: Internalpatientid, Age at ed visit, Ed visit start date, Died during ed visit, First listed diagnosis icd10 subcategory, Second listed diagnosis icd10 subcategory	
-dataset_path = 'ed_visits_train.csv'
-load_path = os.path.join(DATA_PATH, dataset_path)
-ed = pd.read_csv(load_path)
-ed = ed[["Internalpatientid", "Age at ed visit", "Died during ed visit", 
-         "First listed diagnosis icd10 subcategory", "Second listed diagnosis icd10 subcategory"]]
+
