3dc50add9383384dc5a157d9cf3d5fd91d8bff38
b23shen <b23shen@uwaterloo.ca>
2023-07-22 19:53:27 -0400

    add: XGBoost, AdaBoost, LGBM

--- src/models/grid_search_cv.py
+++ src/models/grid_search_cv.py
@@ -14,6 +14,10 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn import metrics   #Additional scklearn functions
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
+
+import xgboost as xgb
+from sklearn.ensemble import AdaBoostClassifier
+from lightgbm import LGBMClassifier
  
 def print_best_score(gsearch,param_test):
      # 输出best score
@@ -83,6 +87,31 @@ RandomForest_param = {
     'min_samples_leaf': list(range(2,7,1)) #also used for prevent overfitting
 }
 
+XGBoost_param = {
+    'learning_rate': [0.1, 0.01],
+    'max_depth': [3, 5, 7],
+    'min_child_weight': [1, 5, 10],
+    'subsample': [0.7, 0.8, 0.9],
+    'colsample_bytree': [0.7, 0.8, 0.9],
+    'n_estimators': [50, 100]
+}
+
+AdaBoost_param = {
+    'learning_rate': [0.1, 0.01],
+    'n_estimators': [50, 100],
+    'base_estimator__max_depth': [3, 5, 7],
+    'base_estimator__min_samples_leaf': [1,5,10]
+}
+
+LGBM_param = {
+    'learning_rate': [0.1, 0.01],
+    'n_estimators': [50, 100],
+    'max_depth': [3, 5, 7],
+    'colsample_bytree': [0.7, 0.8, 0.9],
+    'subsample': [0.7, 0.8, 0.9],
+    'min_child_samples': [1, 5, 10]
+}
+
 def LogisticRegression_Grid_CV(X_train,y_train, LogisticRegression_param):
     estimator = LogisticRegression()
     gsearch = GridSearchCV(estimator , param_grid = LogisticRegression_param, scoring='roc_auc', cv=5)
@@ -101,10 +130,27 @@ def DecisionTree_Grid_CV(X_train,y_train, DecisionTree_param):
     gsearch.fit(X_train, y_train)
     print_best_score(gsearch,DecisionTree_param)
 
-def RandomForest_Grid_CV(X_train,y_train, DecisionTree_param):
+def RandomForest_Grid_CV(X_train,y_train, RandomForest_param):
     estimator = RandomForestClassifier()
     gsearch = GridSearchCV(estimator , param_grid = RandomForest_param, scoring='roc_auc', cv=5)
     gsearch.fit(X_train, y_train)
     print_best_score(gsearch, RandomForest_param)
 
+def XGBoost_Grid_CV(X_train,y_train, XGBoost_param):
+    estimator = xgb.XGBClassifier()
+    gsearch = GridSearchCV(estimator , param_grid = XGBoost_param, scoring='roc_auc', cv=5)
+    gsearch.fit(X_train, y_train)
+    print_best_score(gsearch, XGBoost_param)
 
+def AdaBoost_Grid_CV(X_train,y_train, AdaBoost_param):
+    base = DecisionTreeClassifier()
+    estimator = AdaBoostClassifier(base_estimator = base)
+    gsearch = GridSearchCV(estimator , param_grid = AdaBoost_param, scoring='roc_auc', cv=5)
+    gsearch.fit(X_train, y_train)
+    print_best_score(gsearch, AdaBoost_param)
+
+def LGBM_Grid_CV(X_train,y_train, LGBM_param):
+    estimator = LGBMClassifier()
+    gsearch = GridSearchCV(estimator , param_grid = LGBM_param, scoring='roc_auc', cv=5)
+    gsearch.fit(X_train, y_train)
+    print_best_score(gsearch, LGBM_param)
