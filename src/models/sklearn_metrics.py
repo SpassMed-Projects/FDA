a7e7e15951d7cd870bfd1897767960725d73636e
18cd11 <chen.dan@mail.utoronto.ca>
2023-07-25 10:42:03 -0400

    update:metrics

--- src/models/sklearn_metrics.py
+++ src/models/sklearn_metrics.py
@@ -18,6 +18,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import f1_score
+from sklearn.metrics import confusion_matrix
 
 import numpy as np
 np.random.seed(401)
@@ -28,16 +29,16 @@ def get_AUPRC(y_test, y_score):
     precision, recall, thresholds = precision_recall_curve(y_test, y_score)
     # Use AUC function to calculate the area under the curve of precision recall curve
     auc_precision_recall = auc(recall, precision)
-    print(auc_precision_recall)
+    return auc_precision_recall
     
 
 def get_AUROC(y_test, y_score):
     auc_roc_score = roc_auc_score(y_test, y_score)
-    print(auc_roc_score)
+    return auc_roc_score
 
 def get_Accurarcy(y_test, y_score):
     accuracy = accuracy_score(y_test, y_score)
-    print(accuracy)
+    return accuracy
 
 
 def get_SensitSpecific(y_test, y_score):
@@ -45,23 +46,39 @@ def get_SensitSpecific(y_test, y_score):
 
     #As it was mentioned in the other answers, specificity is the recall of the negative class
     specificity = recall_score(y_test, y_score, pos_label=0) # TN / (TN + FP) 
-    print(sensitivity + specificity)
+    return sensitivity + specificity
 
 def get_Sensitivity(y_test, y_score):
     sensitivity = recall_score(y_test, y_score,average = 'binary') #TP / (TP + FN)
-    print(sensitivity)
+    return sensitivity
 
 def get_Specificity(y_test, y_score):
     specificity = recall_score(y_test, y_score, average = 'binary', pos_label=0) # TN / (TN + FP) 
-    print(specificity)
+    return specificity
 
 def get_Precision(y_test, y_score):
     precision = precision_score(y_test, y_score, average='binary')
-    print(precision)
+    return precision
 
 def get_F1score(y_test, y_score):
     score = f1_score(y_test, y_score, average='binary')
-    print(score)
+    return score
+
+def get_NPC(y_test, y_score):
+    tn, fp, fn, tp = confusion_matrix(y_test, y_score).ravel()
+    npc =  tn / (tn + fn)
+    return npc
+
+def get_PositiveLR(y_test, y_score):
+    # sensitivity / (1 - specificity)
+    positive_lr = get_Sensitivity(y_test, y_score) / (1 - get_Specificity(y_test, y_score))
+    return positive_lr
+
+def get_NegativeLR(y_test, y_score):
+    # (1 - sensitivity) / specificity
+    negative_lr = (1 - get_Sensitivity(y_test, y_score)) / get_Specificity(y_test, y_score)
+    return negative_lr
+    
 
 
 ## 不知道有没有用：
